18617e0c723f8d5ed248a1fd62f717163d371973
Yuma <yuma.ollivier@hotmail.com>
2022-03-25 19:39:40 +0100

    ERROR - error handling is taken into consideration

--- app.js
+++ app.js
@@ -91,18 +91,18 @@ postgresDb
     app.use(adminRoutes);
     app.use(authRoutes);
 
-    // app.get('/500', errorController.get500);
+    app.get('/500', errorController.get500);
 
-    // app.use(errorController.get404);
-    // app.use((error, req, res, next) => {
-    //   console.error(error);
-    //   res.status(500).render('500', {
-    //     pageTitle: 'Erreur !',
-    //     path: '/500',
-    //     connected: req.session?.isLoggedIn,
-    //     user: req.user,
-    //   });
-    // });
+    app.use(errorController.get404);
+    app.use((error, req, res, next) => {
+      console.error(error);
+      res.status(500).render('500', {
+        pageTitle: 'Erreur !',
+        path: '/500',
+        connected: req.session.isLoggedIn,
+        user: req.user,
+      });
+    });
 
     const port = process.env.PORT || 3000;
     app.listen(port, () => {
