77e5c9872d924e2377b9037fef540c832713ce16
Yuma <yuma.ollivier@hotmail.com>
2022-03-30 19:34:51 +0200

    BACKARROW - try to fix back link

--- controllers/error.js
+++ controllers/error.js
@@ -1,23 +1,27 @@
 const Users = require('../models/users');
 
 exports.get404 = (req, res, next) => {
-  Users.findOne({where : { id: req.session.user?.id }}).then(user => {
-    res.status(404).render('404', {
-      pageTitle: 'Page introuvable',
-      path: '',
-      connected: req.session.isLoggedIn,
-      prevPath: undefined,
+  if (req.session.user) {
+    Users.findOne({ where: { id: req.session.user.id } }).then(user => {
+      res.status(404).render('404', {
+        pageTitle: 'Page introuvable',
+        path: '',
+        connected: req.session.isLoggedIn,
+        prevPath: undefined,
+      });
     });
-  });
+  }
 };
 
 exports.get500 = (req, res, next) => {
-  Users.findOne({where : { id: req.session.user?.id }}).then(user => {
-    res.status(500).render('500', {
-      pageTitle: 'Erreur \!',
-      path: '/500',
-      connected: req.session.isLoggedIn,
-      prevPath: undefined,
+  if (req.session.user) {
+    Users.findOne({ where: { id: req.session.user.id } }).then(user => {
+      res.status(500).render('500', {
+        pageTitle: 'Erreur !',
+        path: '/500',
+        connected: req.session.isLoggedIn,
+        prevPath: undefined,
+      });
     });
-  });
+  }
 };
